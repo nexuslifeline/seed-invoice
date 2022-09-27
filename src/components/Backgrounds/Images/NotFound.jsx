e2b81d4f41a09e8db026de4a41b7215a746db8f4
edprnl <pecsonerick.ep@gmail.com>
2022-09-27 17:24:07 +0800

    not found page
    not found page

---
+++ src/components/Backgrounds/Images/NotFound.jsx
@@ -0,0 +1,6 @@
+import SVG from 'react-inlinesvg';
+import ImageError404 from 'shared/assets/images/error-404.svg';
+
+const NotFound = (props) => <SVG src={ImageError404} {...props} />;
+
+export default NotFound;
--- src/views/NotFound/index.jsx
+++ src/views/NotFound/index.jsx
@@ -1,5 +1,15 @@
+import ImageNotFound from 'components/Backgrounds/Images/NotFound';
+import Styles from './NotFound.module.scss';
+
 const NotFound = (props) => {
-  return <h1>{`NotFound`}</h1>;
+  return (
+    <div className={Styles.container}>
+      <ImageNotFound className={Styles.bgImage} />
+      <p>
+        Sorry, the page you requested cannot be found. Please go back to homepage or contact us ay support@support.com
+      </p>
+    </div>
+  );
 };
 
 export default NotFound;
