af40e0d193ab8d12923f3de01c128be0c2aa4237
Regin Calaguas <regin_calaguas@yahoo.com>
2023-07-22 19:22:05 -0700

    Update Container.jsx
    Added 2 additional parameters

--- src/components/Table/Container.jsx
+++ src/components/Table/Container.jsx
@@ -1,13 +1,18 @@
 import Styles from './Container.module.scss';
 import Text from 'components/Text';
 
-const Container = ({ children }) => {
+const Container = ({ children, containerTitle, containerDesc }) => {
   return (
     <div className={Styles.container}>
       <div className={Styles.header}>
-        <Text>{'Invoices'}</Text>
+
+        {/* <Text>{'Invoices'}</Text> */}
+        <Text>{containerTitle}</Text>
         <Text variant='description'>
-          {'List of invoices created by members'}
+
+          {/* {'List of invoices created by members'} */}
+          {containerDesc}
+
         </Text>
       </div>
       <div className={Styles.body}>{children}</div>
