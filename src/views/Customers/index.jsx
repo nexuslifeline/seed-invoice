d332bed1646a1a74884eff5a2fe057be20a98e27
Regin Calaguas <regin_calaguas@yahoo.com>
2023-07-24 18:29:57 -0700

    Update index.jsx

--- src/views/Customers/index.jsx
+++ src/views/Customers/index.jsx
@@ -10,9 +10,10 @@ import EditProfile from 'components/Icons/EditProfile';
 import { useForm } from 'react-hook-form';
 
 
+
 const Customers = (props) => {
 
-  const { register, handleSubmit, setValue } = useForm({
+  const { register, handleSubmit, setValue, reset } = useForm({
     defaultValues: {
       firstName: '',
       lastName: '',
@@ -20,21 +21,6 @@ const Customers = (props) => {
       address: '',
     }
   });
-
-  const handleEditClick = (row) => {
-    const { id } = row.original;
-    const { name } = row.original;
-    const { age } = row.original;
-    const { address } = row.original;
-
-    setValue('firstName', name);
-    setValue('lastName', name);
-    setValue('age', age);
-    setValue('address', address);
-
-    setIsModalOpen(true);
-    console.log('Edit button clicked for customer:', id + " " + name);
-  };
 
   const columns = useMemo(
     () => [
@@ -69,14 +55,23 @@ const Customers = (props) => {
         accessor: 'action',
         thStyle: { width: '80px', minWidth: '80px' },
         Cell: ({ row }) => (
-          <button onClick={() => handleEditClick(row)} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <button onClick={() => {
+            setValue('firstName', row.original.name);
+            setValue('lastName', row.original.name);
+            setValue('age', row.original.age);
+            setValue('address', row.original.address);
+            setIsModalOpen(true);
+          }}
+            style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <EditProfile style={{ width: '24px', height: '24px', fill: 'white' }} />
           </button>
         ),
       },
     ],
-    []
+    [setValue]
   );
+
+
 
   const [isLoading, setIsLoading] = useState(false);
   const [tableData, setTableData] = useState([]);
@@ -111,6 +106,7 @@ const Customers = (props) => {
 
   const closeModal = () => {
     setIsModalOpen(false);
+    reset();
   };
 
   const Modal = ({ isOpen, onClose, children }) => {
@@ -162,7 +158,68 @@ const Customers = (props) => {
       <Modal isOpen={isModalOpen} onClose={closeModal}>
         <h2>Customer Information</h2>
         <p>Modal content goes here.</p>
-        <form onSubmit={handleSubmit((data) => {
+
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
+          <div style={{ display: 'flex', flexDirection: 'row', gap: '20px' }}>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+
+              <input {...register("firstName", {
+                required: 'This is required.',
+                minLength: {
+                  value: 3,
+                  message: 'Min Length is 3'
+                }
+              })} id="firstName" placeholder='First Name' />
+
+              <input {...register("middleName")} id="middleName" placeholder='Middle Name' />
+
+
+              <input {...register("lastName")} id="lastName" placeholder='Last Name' />
+
+
+              <input {...register("age")} id="age" placeholder='Age' />
+            </div>
+
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+
+              <input {...register("displayName")} id="displayName" placeholder='Display Name' />
+
+
+              <input {...register("company")} id="company" placeholder='Company' />
+
+
+              <input {...register("email")} id="email" placeholder='Email' />
+
+
+              <input {...register("phone")} id="phone" placeholder='Phone' />
+
+
+              <input {...register("mobile")} id="mobile" placeholder='Mobile' />
+
+
+              <input {...register("fax")} id="fax" placeholder='Fax' />
+
+
+              <input {...register("website")} id="website" placeholder='Website' />
+
+
+              <input {...register("other")} id="other" placeholder='Other' />
+
+
+              <input {...register("address")} id="address" placeholder='Address' />
+            </div>
+          </div>
+
+          <button onClick={closeModal}>Cancel</button>
+          <input type='submit' />
+        </form>
+
+
+        {/* <form onSubmit={handleSubmit((data) => {
           console.log(data);
         })}>
           <input {...register("firstName", {
@@ -175,7 +232,7 @@ const Customers = (props) => {
           <input {...register("middleName")} placeholder='Middle Name'></input>
           <input {...register("lastName")} placeholder='Last Name'></input>
           <input {...register("age")} placeholder='Age'></input>
-{/*           <input {...register("displayName")} placeholder='Display Name'></input>
+          <input {...register("displayName")} placeholder='Display Name'></input>
           <input {...register("company")} placeholder='Company'></input>
           <input {...register("email")} placeholder='Email'></input>
           <input {...register("phone")} placeholder='Phone'></input>
@@ -183,10 +240,10 @@ const Customers = (props) => {
           <input {...register("fax")} placeholder='Fax'></input>
           <input {...register("website")} placeholder='Website'></input>
           <input {...register("other")} placeholder='Other'></input>
-          <input {...register("address")} placeholder='Address'></input> */}
+          <input {...register("address")} placeholder='Address'></input>
           <button onClick={closeModal}>Cancel</button>
           <input type='submit'></input>
-        </form>
+          </form> */}
 
       </Modal>
 
