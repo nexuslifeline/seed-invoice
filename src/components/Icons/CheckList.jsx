0a2401e05ce20c5065d00bdbf51474a53184a766
Paul Christian Rueda <nexuslifeline@gmail.com>
2024-02-04 16:50:37 +0800

    Reorganize Navs and Add Unit and Payment terms navs.

---
+++ src/components/Icons/CheckList.jsx
@@ -0,0 +1,6 @@
+import SVG from 'react-inlinesvg';
+import IconCheckList from '@shared/assets/icons/check-list.svg';
+
+const CheckList = (props) => <SVG src={IconCheckList} {...props} />;
+
+export default CheckList;
--- src/router/nav.js
+++ src/router/nav.js
@@ -1,85 +1,88 @@
-import Apps from "@components/Icons/Mono/Apps";
-import Gear from "@components/Icons/Mono/Gear";
-import Star from "@components/Icons/Star";
-import Circles from "@components/Icons/Mono/Circles";
-import Graph from "@components/Icons/Mono/Graph";
-import Box from "@components/Icons/Box";
-import AddGroup from "@components/Icons/AddGroup";
-import Building from "@components/Icons/Building";
-import Tax from "@components/Icons/Tax";
-import Bag from "@components/Icons/ShoppingBag";
-import Wallet from "@components/Icons/Wallet";
-import Invoice from "@components/Icons/Invoice";
-import CreditCard from "@components/Icons/CreditCard";
-import Team from "@components/Icons/Team";
-import Subfolder from "@components/Icons/Subfolder";
-import EditProfile from "@components/Icons/EditProfile";
-import Options from "@components/Icons/Options";
-import Vibration from "@components/Icons/Vibration";
-import Home from "@components/Icons/Mono/Home";
-import Role from "@components/Icons/Role";
-import Bill from "@components/Icons/Bill";
-import SalesIncrease from "@components/Icons/SalesIncrease";
+import Apps from '@components/Icons/Mono/Apps';
+import Gear from '@components/Icons/Mono/Gear';
+import Star from '@components/Icons/Star';
+import Circles from '@components/Icons/Mono/Circles';
+import Graph from '@components/Icons/Mono/Graph';
+import Box from '@components/Icons/Box';
+import AddGroup from '@components/Icons/AddGroup';
+import Building from '@components/Icons/Building';
+import Tax from '@components/Icons/Tax';
+import Bag from '@components/Icons/ShoppingBag';
+import Wallet from '@components/Icons/Wallet';
+import Invoice from '@components/Icons/Invoice';
+import CreditCard from '@components/Icons/CreditCard';
+import Team from '@components/Icons/Team';
+import Subfolder from '@components/Icons/Subfolder';
+import EditProfile from '@components/Icons/EditProfile';
+import Options from '@components/Icons/Options';
+import Vibration from '@components/Icons/Vibration';
+import Home from '@components/Icons/Mono/Home';
+import Role from '@components/Icons/Role';
+import Bill from '@components/Icons/Bill';
+import SalesIncrease from '@components/Icons/SalesIncrease';
+import CheckList from '@components/Icons/CheckList';
 
 export const navLinks = [
-  { text: "Dashboard", icon: Home, to: "/dashboard", groups: [] },
+  { text: 'Dashboard', icon: Home, to: '/dashboard', groups: [] },
   {
-    text: "Home",
+    text: 'Home',
     icon: Apps,
     groups: [
       {
-        text: "Workspace",
+        text: 'Workspace',
         children: [
-          { text: "Quotes", to: "/quotes", icon: Star },
-          { text: "Sales Order", to: "/orders", icon: Bag },
-          { text: "Invoices", to: "/invoices", icon: Invoice },
-          { text: "Payments", to: "/payments", icon: Wallet },
-          { text: "Expenses", to: "/expenses", icon: CreditCard }
+          { text: 'Quotes', to: '/quotes', icon: Star },
+          { text: 'Sales Order', to: '/orders', icon: Bag },
+          { text: 'Invoices', to: '/invoices', icon: Invoice },
+          { text: 'Payments', to: '/payments', icon: Wallet },
+          { text: 'Expenses', to: '/expenses', icon: CreditCard }
         ]
       }
     ]
   },
   {
-    text: "References",
+    text: 'References',
     icon: Circles,
     groups: [
       {
-        text: "Management",
+        text: 'Management',
         children: [
-          { text: "Customers", to: "/customers", icon: AddGroup },
-          { text: "Products", to: "/products", icon: Box },
-          { text: "Categories", to: "/categories", icon: Subfolder },
-          { text: "Payment Modes", to: "/payment-modes", icon: Vibration },
-          { text: "Tax", to: "/tax", icon: Tax }
+          { text: 'Products', to: '/products', icon: Box },
+          { text: 'Customers', to: '/customers', icon: AddGroup },
+          { text: 'Unit', to: '/unit', icon: CheckList },
+          { text: 'Categories', to: '/categories', icon: Subfolder }
         ]
       }
     ]
   },
   {
-    text: "Reports",
+    text: 'Reports',
     icon: Graph,
     groups: [
       {
-        text: "Reports",
+        text: 'Reports',
         children: [
-          { text: "Sales Report", to: "/sales-report", icon: SalesIncrease }
+          { text: 'Sales Report', to: '/sales-report', icon: SalesIncrease }
         ]
       }
     ]
   },
   {
-    text: "Settings",
+    text: 'Settings',
     icon: Gear,
     groups: [
       {
-        text: "Settings",
+        text: 'Settings',
         children: [
-          { text: "Company", to: "/company", icon: Building },
-          { text: "Account", to: "/account", icon: EditProfile },
-          { text: "Preferences", to: "/preferences", icon: Options },
-          { text: "Members", to: "/members", icon: Team },
-          { text: "Roles", to: "/roles", icon: Role },
-          { text: "Billing", to: "/billing", icon: Bill }
+          { text: 'Company', to: '/company', icon: Building },
+          { text: 'Account', to: '/account', icon: EditProfile },
+          { text: 'Preferences', to: '/preferences', icon: Options },
+          { text: 'Members', to: '/members', icon: Team },
+          { text: 'Roles', to: '/roles', icon: Role },
+          { text: 'Billing', to: '/billing', icon: Bill },
+          { text: 'Payment Terms', to: '/payment-terms', icon: CreditCard },
+          { text: 'Payment Modes', to: '/payment-modes', icon: Vibration },
+          { text: 'Tax', to: '/tax', icon: Tax }
         ]
       }
     ]
