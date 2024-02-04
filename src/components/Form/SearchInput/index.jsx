c0d6efdc9c446aa79caec3231e7cd985140533cc
Paul Christian Rueda <nexuslifeline@gmail.com>
2024-02-04 12:04:45 +0800

    Fix various issues in Search input component

--- src/components/Form/SearchInput/index.jsx
+++ src/components/Form/SearchInput/index.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
-import Styles from './SearchInput.module.scss';
 import classNames from 'classnames';
-import MagnifyingGlass from '@components/Icons/MagnifyingGlass';
 
-const SearchInput = ({ label, containerClassName, ...props }) => {
-  return (
-    <div className={classNames(Styles.container, containerClassName)}>
-      <MagnifyingGlass className={Styles.icon} />
-      <input type='text' className={Styles.input} {...props} />
-    </div>
-  );
-};
+import MagnifyingGlass from '@components/Icons/MagnifyingGlass';
+import InvalidFeedback from '@components/InvalidFeedback';
+
+import Styles from './SearchInput.module.scss';
+
+const SearchInput = forwardRef(
+  ({ label, error, containerClassName, ...props }, ref) => {
+    return (
+      <div className={Styles.container}>
+        <div className={classNames(Styles.searchContainer, containerClassName)}>
+          <MagnifyingGlass className={Styles.icon} />
+          <input ref={ref} type='text' className={Styles.input} {...props} />
+        </div>
+        {error && <InvalidFeedback error={error} />}
+      </div>
+    );
+  }
+);
 
 SearchInput.propTypes = {
   label: PropTypes.string,
   containerClassName: PropTypes.string,
+  error: PropTypes.string
 };
+
+SearchInput.InstanceOf = 'SearchInput';
 
 export default SearchInput;
