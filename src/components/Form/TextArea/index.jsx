a161b90baee83e4663246d2474639d55cb7bcff4
Paul Christian Rueda <nexuslifeline@gmail.com>
2024-02-04 08:16:21 +0800

    Add react-hook-form support to custom TextArea component. Refactor form logic when injecting props

--- src/components/Form/TextArea/index.jsx
+++ src/components/Form/TextArea/index.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import Styles from './TextArea.module.scss';
 import classNames from 'classnames';
 import InvalidFeedback from '@components/InvalidFeedback';
 
-const TextArea = ({ label, containerClassName, error, hideLabel = true, ...props }) => {
-  return (
-    <div className={classNames(Styles.container, containerClassName)}>
-      <textarea className={Styles.input} {...props} placeholder=' ' />
-      {!hideLabel && <label className={Styles.label}>{label}</label>}
-      {error && <InvalidFeedback error={error} />}
-    </div>
-  );
-};
+const TextArea = forwardRef(
+  ({ label, containerClassName, error, hideLabel = true, ...props }, ref) => {
+    return (
+      <div className={classNames(Styles.container, containerClassName)}>
+        <textarea
+          ref={ref}
+          className={Styles.input}
+          {...props}
+          placeholder=' '
+        />
+        {!hideLabel && <label className={Styles.label}>{label}</label>}
+        {error && <InvalidFeedback error={error} />}
+      </div>
+    );
+  }
+);
 
 TextArea.propTypes = {
   label: PropTypes.string,
   containerClassName: PropTypes.string,
-  error: PropTypes.string,
+  error: PropTypes.string
 };
+
+TextArea.InstanceOf = 'TextArea';
 
 export default TextArea;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,6 +7,10 @@ import TextInput from './TextInput';
 import Select from './Select';
 import Field from './Field';
 import { capitalize } from '@lib/utils';
+import PhoneNumber from './PhoneNumber';
+import PasswordInput from './PasswordInput';
+import Toggle from './Toggle';
+import SearchInput from './SearchInput';
 
 const Form = ({ children, onSubmit, defaultValues = {} }) => {
   const {
@@ -19,70 +23,90 @@ const Form = ({ children, onSubmit, defaultValues = {} }) => {
 
   const makeFieldError = ({
     fieldName,
+    fieldLabel,
     minLength,
     maxLength,
     min,
     max,
     error
   }) => {
+    const field = fieldLabel || fieldName;
     return (
       {
-        required:
-          error?.message || `${capitalize(fieldName)} field is required.`,
+        required: error?.message || `${capitalize(field)} field is required.`,
         minLength:
           error?.message ||
           `The ${capitalize(
-            fieldName
+            field
           )} field must be at least ${minLength} characters long.`,
         maxLength:
           error?.message ||
           `The ${capitalize(
-            fieldName
+            field
           )} field must not exceed ${maxLength} characters in length.`,
-        max: error?.message || `The ${fieldName} field must not exceed ${max}.`,
-        min: error?.message || `The ${fieldName} field must be atleast ${min}.`,
-        pattern: `The ${fieldName} field must match the expected pattern.`
-      }?.[error?.type] || 'There was an error in this field.'
+        max: error?.message || `The ${field} field must not exceed ${max}.`,
+        min: error?.message || `The ${field} field must be atleast ${min}.`,
+        pattern: `The ${field} field must match the expected pattern.`
+      }?.[error?.type] || 'Please enter a valid value.'
     );
+  };
+
+  const createSelectProps = (child) => {
+    return {
+      onChange: (option) => {
+        setValue(name, option); // set value in form so that react-hook-form can validate
+        clearErrors(name); // clear errors in react-hook-form
+        child?.props?.onChange?.(option); // then call onChange on child
+      }
+    };
+  };
+
+  const injectProps = (child, key) => {
+    const { rule, name, label } = child.props || {};
+
+    const hasRule = Boolean(Object.keys(rule || {})?.length);
+
+    if (!hasRule) return child;
+
+    // Spread register only if rule has a value
+    const rcfProps = { ...register(name, rule) };
+
+    // Combine rcfProps, errorProps and other custom component specific props when cloning the element
+    let mergedProps = { key: child?.key || key, ...rcfProps };
+
+    // If errors exist, add error props
+    const hasErrors = Boolean(errors?.[name]);
+    if (hasErrors) {
+      const errorProps = {
+        error: makeFieldError({
+          fieldName: name,
+          fieldLabel: label,
+          error: errors?.[name],
+          ...rule
+        })
+      };
+      mergedProps = { ...mergedProps, ...errorProps };
+    }
+
+    // Check if child is an instance of Select, if so, add other props
+    if (child?.type?.InstanceOf === 'Select') {
+      mergedProps = { ...mergedProps, ...createSelectProps(child) };
+    }
+
+    return cloneElement(child, mergedProps);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      {children?.map((child, idx) => {
-        const { rule, name } = child.props || {};
-
-        // Spread register only if rule has a value
-        const registerProps = { ...register(name, rule) };
-        const errorProps = {
-          error: makeFieldError({
-            fieldName: name,
-            error: errors?.[name],
-            ...rule
-          })
-        };
-
-        // Check if child is a Select, if so, add onChange
-        const isSelect = child?.type?.InstanceOf === 'Select';
-        const selectProps = {
-          onChange: (option) => {
-            setValue(name, option); // set value in form so that react-hook-form can validate
-            clearErrors(name); // clear errors in react-hook-form
-            child?.props?.onChange?.(option); // then call onChange on child
-          }
-        };
-
-        // Combine registerProps and errorProps when cloning the element
-        return cloneElement(child, {
-          key: child?.key || idx,
-          ...(rule && registerProps),
-          ...(isSelect && selectProps),
-          ...(errors?.[name] && errorProps)
-        });
-      })}
+      {children?.map((child, idx) => injectProps(child, idx))}
     </form>
   );
 };
 
+Form.SearchInput = SearchInput;
+Form.Toggle = Toggle;
+Form.PasswordInput = PasswordInput;
+Form.PhoneNumber = PhoneNumber;
 Form.TextArea = TextArea;
 Form.TextInput = TextInput;
 Form.Select = Select;
@@ -91,5 +115,7 @@ Form.Field = Field;
 Form.propTypes = {
   children: PropTypes.node
 };
+
+Form.InstanceOf = 'Form';
 
 export default Form;
